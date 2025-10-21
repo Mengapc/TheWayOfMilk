1719a656aaa6316b76cbef906ca5cf3a0509c593
Mengapc <gabriel@mengarelli.com.br>
2025-10-21 20:25:47 -0300

    Atualizando a skybox

--- BaseProject/Assets/_Project/Scripts/Geral/GeralPuzzles/DestructionBalls.cs
+++ BaseProject/Assets/_Project/Scripts/Geral/GeralPuzzles/DestructionBalls.cs
@@ -5,8 +5,6 @@ public class DestructionBalls : MonoBehaviour
     [Header("References")]
     [Tooltip("Collider que detecta a colisão com as bolas para destruí-las.")]
     [SerializeField] private Collider destructCollider;
-    [Tooltip("Referência ao script SpanwBalls para controlar o número de bolas spawnadas(sala 2).")]
-    [SerializeField] private SpanwBalls spanwBalls;
     [Tooltip("Referência ao script BallController para acessar propriedades da bola.")]
     [SerializeField] private BallController ballController;
  
@@ -22,10 +20,11 @@ public class DestructionBalls : MonoBehaviour
                 Instantiate(ballController.ballPrefab, ballController.spawnPoint.position, ballController.spawnPoint.rotation);
             }
             */
-            if (ballController.roomIdentifier == RoomIdentifier.Sala2)
+            /*if (ballController.roomIdentifier == RoomIdentifier.Sala2)
             {
                 spanwBalls.spawnedBalls--;
             }
+            */
             Destroy(ballController);
             Destroy(other);
         }
