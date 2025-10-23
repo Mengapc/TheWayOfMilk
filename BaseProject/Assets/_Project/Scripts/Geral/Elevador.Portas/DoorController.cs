2ef78f84cbc5cf5dc29b222a0a9f04ff13bcdfd2
Gabriel d <gabrielsilvapereiradias@gmail.com>
2025-10-22 22:21:47 -0300

    arrumando salas

--- BaseProject/Assets/_Project/Scripts/Geral/Elevador.Portas/DoorController.cs
+++ BaseProject/Assets/_Project/Scripts/Geral/Elevador.Portas/DoorController.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using System.Collections;
 
 public class DoorController : MonoBehaviour
 {
-    [SerializeField] private GameObject door; 
+    [SerializeField] private GameObject door;
+    [SerializeField] private AnimationCurve _animationCurve;
+    [SerializeField] float initialRotation;
+    [SerializeField] float atualRotation;
+    [SerializeField] float endRotation;
+    [SerializeField] private float duration;
+    [SerializeField] private float speed;
+    [SerializeField] private SkinnedMeshRenderer doorRenderer;
     private bool isOpen = false;
+
 
     public void OpenDoor()
     {
         if (!isOpen)
         {
-            isOpen = true;
-            door.SetActive(false); 
+            StartCoroutine(AnimationOpen(initialRotation,endRotation));
+        }
+        else
+        {
+            star
+        }
+    }
+    private IEnumerator AnimationOpen(float startValue, float endValue)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+            float curveValue = _animationCurve.Evaluate(t);
+            atualRotation = Mathf.Lerp(startValue, endValue, curveValue) * 100f;
+            transform.rotation = new Quaternion(ne);
+            elapsedTime += Time.deltaTime;
+            yield return null;
         }
     }
 }
