91588a8920d604025fd4bd1ae2e76dac2ecd4c60
Moonguito <guialvesteixeira01@gmail.com>
2025-11-05 02:56:02 -0300

    Colliders, Pedras e ajustes menores

--- BaseProject/Assets/_Project/Scripts/Player/LaizerDestroy.cs
+++ BaseProject/Assets/_Project/Scripts/Player/LaizerDestroy.cs
@@ -1,9 +1,32 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class LaizerDestroy : MonoBehaviour
 {
-    public void SelfeDestroy()
+    [Header("Objetos a desligar na ordem")]
+    public List<GameObject> objectsToDisable;
+
+    [Header("Delay entre cada um")]
+    public float delayBetween = 0.2f;
+
+    // pode chamar por evento, trigger, animação, etc
+    public void StartDestroySequence()
     {
+        StartCoroutine(DestroySequence());
+    }
+    IEnumerator DestroySequence()
+    {
+        // desativa um por um
+        for (int i = 0; i < objectsToDisable.Count; i++)
+        {
+            if (objectsToDisable[i] != null)
+                objectsToDisable[i].SetActive(false);
+
+            yield return new WaitForSeconds(delayBetween);
+        }
+
+        // no fim, destrói o objeto que tem esse script
         Destroy(gameObject);
     }
 }
